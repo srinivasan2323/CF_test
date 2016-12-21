a5d8bb1a360b496bde30a41a23dc9e9010a2fe7b
srinivasan.sankaran <srinivasan.sankaran@BDC3-D-519QSWM.dir.svc.accenture.com>
2016-12-21 16:56:31 +0530

    Commit sample1

---
+++ src/com/sample/Sampl.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+public class Sampl {
+private String rollno;
+
+public String getRollno() {
+	return rollno;
+}
+
+public void setRollno(String rollno) {
+	this.rollno = rollno;
+}
+}
